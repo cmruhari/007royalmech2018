83bad61af28c2e6b657aee5092ba5d7a9fa2a5bb
cmruhari <42933128+cmruhari@users.noreply.github.com>
2018-09-17 15:42:16 +0530

    Add files via upload

---
+++ check.py
@@ -0,0 +1,7 @@
+num=int(input("enter the number:"))
+if(num>0):
+   print("number is positive")
+else:
+   print("number is negative")
+   #to check number is +ve or -ve
+   
---
+++ marks.py
@@ -0,0 +1,13 @@
+marks=int(input("enter the marks:"))
+if(marks>=90):
+     print("excellent")
+elif(marks>=85):
+     print("v.good")
+elif(marks>=70):
+     print("good")
+elif(marks>=60):
+     print("average")
+else:
+     print("below or fail")     
+
+    
