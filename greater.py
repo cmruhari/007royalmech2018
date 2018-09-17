2ecb9f6bd1d0d036383ee4f32c973d7af7af55f3
cmruhari <42933128+cmruhari@users.noreply.github.com>
2018-09-17 16:11:08 +0530

    Add files via upload

---
+++ greater.py
@@ -0,0 +1,9 @@
+#&&=logical and
+a=int(input("enter the value for a:"))
+b=int(input("enter the value for b:"))
+if(a>b):
+  print("a is greater")
+elif(b>a):
+   print("b is greater")
+else:
+   print("both are equal")     
