f08bf0d303373a569d940900e53ee1384af2a26c
cmruhari <42933128+cmruhari@users.noreply.github.com>
2018-09-24 16:27:41 +0530

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,16 @@
+import random
+while True:
+	a=input("enter "r" to roll die")
+	if(a==r):
+		r=random.randint(1,6)
+		print(r)
+	else:
+	    print("wrong key")
+	    break	
+
+   
+   
+   
+   
+
+
