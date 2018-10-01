4b57a135ec9e9cdc9f5e4f36c1ac0beebd3773be
cmruhari <42933128+cmruhari@users.noreply.github.com>
2018-10-01 16:18:15 +0530

    Add files via upload

--- rock.py
+++ rock.py
@@ -1,13 +1,34 @@
 import random
 
-a={1:'r',2:'p',3:'s'}
+a={1:'rock',2:'paper',3:'scissor'}
 
 while True:
-	p=input("your choice r/p/s")
+	p=input('enter your choice:')
 
 	c=a[random.randint(1,3)]
 
-	print("you choose:",p,"you choose:",c)
+	print("you chose:",p,"\ncomputer chose:",c)
+
+	if(p==c):
+		print('Draw!  ')
+	elif(p=='rock' and c=='paper'):
+		print('you lose! ')
+	elif(p=='rock' and c=='scissor'):
+		print(' you win! ')
+	elif(p=='paper' and c=='rock'):
+		print(' you win! ')
+	elif(p=='paper' and c=='scissor'):
+		print(' you lose!')
+	elif(p=='scissor' and c=='rock'):
+		print(' you lose! ')
+	elif(p=='scissor' and c=='paper'):
+		print('you win!  ')
+	else:
+		print(' bored?')
+		exit()		
+
+
+
 	
 
 
