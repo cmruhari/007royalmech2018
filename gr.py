daef5cdfbb005f59f5d9a170646924cd0a88416d
cmruhari <42933128+cmruhari@users.noreply.github.com>
2018-11-02 11:25:51 +0530

    Add files via upload

---
+++ gr.py
@@ -0,0 +1,17 @@
+
+# importing the required module 
+import matplotlib.pyplot as plt
+  # x axis values 
+x = [1,1.1,1.3] 
+# corresponding y axis values 
+y = [0.002,0.0044,0.008] 
+  # plotting the points  
+plt.plot(x, y) 
+  # naming the x axis 
+plt.xlabel('x - axis') 
+# naming the y axis 
+plt.ylabel('y - axis') 
+  # giving a title to my graph 
+plt.title('My first graph!') 
+  # function to show the plot 
+plt.show() 
